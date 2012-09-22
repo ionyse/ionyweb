7fb513da39cabad5c7314521c89bb69d8561f673
Rémy HUBSCHER <remy.hubscher@novapost.fr>
2012-09-22 11:43:53 +0200

    Fix documentation configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# django-downloadview documentation build configuration file, created by
+# ionyweb documentation build configuration file, created by
 # sphinx-quickstart on Mon Aug 27 11:37:23 2012.
 #
 # This file is execfile()d with the current directory set to its containing dir.
@@ -47,8 +47,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'django-downloadview'
-copyright = u'2012, Benoît Bryon'
+project = u'ionyweb'
+copyright = u'2012, Ionyse'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -171,7 +171,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'django-downloadviewdoc'
+htmlhelp_basename = 'ionywebdoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -190,8 +190,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'django-downloadview.tex', u'django-downloadview Documentation',
-   u'Benoît Bryon', 'manual'),
+  ('index', 'ionyweb.tex', u'ionyweb Documentation',
+   u'Ionyse', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -220,8 +220,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'django-downloadview', u'django-downloadview Documentation',
-     [u'Benoît Bryon'], 1)
+    ('index', 'ionyweb', u'ionyweb Documentation',
+     [u'Ionyse'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -234,8 +234,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'django-downloadview', u'django-downloadview Documentation',
-   u'Benoît Bryon', 'django-downloadview', 'One line description of project.',
+  ('index', 'ionyweb', u'ionyweb Documentation',
+   u'Ionyse', 'ionyweb', 'One line description of project.',
    'Miscellaneous'),
 ]
 
